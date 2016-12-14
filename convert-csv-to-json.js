27fe8e7eaf82118a6d9857d39dff49f4bc6c619a
Ali Gajani <aligajani@gmail.com>
2016-12-14 01:32:57 +0000

    Exporting CSV to JSON, cleaning up etc

---
+++ convert-csv-to-json.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var Converter = require("csvtojson").Converter;
+
+var converter = new Converter({});
+converter.fromFile("fake-news-source.csv", function (error, result) {
+
+    var collection = [];
+    const totalItems = result.length;
+
+    for (var key in result) {
+        if (result.hasOwnProperty(key)) {
+            var item = result[key];
+
+            var intermediate = {
+                siteTitle: item['site'],
+                siteCategory: item['category'],
+                siteUrl: item['URL'],
+                siteGoogleHits: parseInt(item['Google hits']),
+                siteFacebookLikes: parseInt(item['FB likes']),
+                siteTwitterFollowers: parseInt(item['Twitter']),
+                sitePoliticalAlignment: item['political alignment?'],
+                siteNotes: item['Notes']
+            }
+
+            collection.push(intermediate);
+        }
+    }
+    var clean = collection.splice(1,totalItems);
+    writeToFile(JSON.stringify(clean));
+});
+
+
+function writeToFile(data) {
+    fs.writeFile("fake-news-source.json", data, "utf8", function (err) {
+        if (err) return console.log(err);
+        console.log("The file was saved!");
+    });
+}
