d4b9ff8b7fa6a2beda46cb3c3a27b6ff1253aae7
Ali Gajani <aligajani@gmail.com>
2016-12-14 03:02:30 +0000

    Bug fixes and full output dump

--- src/convert-csv-to-json.js
+++ src/convert-csv-to-json.js
@@ -15,9 +15,10 @@ converter.fromFile("./input/fake-news-source.csv", function (error, result) {
                 siteTitle: item['site'],
                 siteCategory: item['category'],
                 siteUrl: item['URL'],
-                siteGoogleHits: parseInt(item['Google hits']),
-                siteFacebookLikes: parseInt(item['FB likes']),
-                siteTwitterFollowers: parseInt(item['Twitter']),
+                siteGoogleHits: convertToNumber(item['Google hits']),
+
+                siteFacebookLikes: convertToNumber(item['FB likes']),
+                siteTwitterFollowers: convertToNumber(item['Twitter']),
                 sitePoliticalAlignment: item['political alignment?'],
                 siteNotes: item['Notes']
             }
@@ -26,7 +27,6 @@ converter.fromFile("./input/fake-news-source.csv", function (error, result) {
         }
     }
     var clean = collection.splice(1,totalItems);
-    console.log(collection.length);
     writeToFile(JSON.stringify(clean));
 });
 
@@ -36,4 +36,9 @@ function writeToFile(data) {
         if (err) return console.log(err);
         console.log("The file was saved!");
     });
+}
+
+function convertToNumber(stringVal) {
+    var converted = String(stringVal).replace(/,/g ,"");
+    return parseInt(converted);
 }
